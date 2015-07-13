df978b4ccc71f9e8a1f6b8788c23ff0e74c7f42e
Dingkang Wang <wdk@wdk.local>
2015-07-13 14:42:33 -0400

    Initial Commit

---
+++ OSU Printer/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  OSU Printer
+//
+//  Created by Dingkang Wang on 7/13/15.
+//  Copyright (c) 2015 Dingkang Wang. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
